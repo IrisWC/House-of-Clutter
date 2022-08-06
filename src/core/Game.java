cd065b630846e51b4e58c009300f5b0b53cf71bf
Iris Chou <iris.w.chou@gmail.com>
2022-08-05 21:28:21 -0700

    add draw stuff

--- src/core/Game.java
+++ src/core/Game.java
@@ -1,16 +1,56 @@
 package core;
 
+import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
+import assets.*;
 
 public class Game extends JPanel implements MouseListener {
 	
 	public static final int WIDTH = 1600;
 	public static final int HEIGHT = 900;
 	
+	private ArrayList<Furniture> furniture;
+	
 	public Game() {
+		super();
 		
+		furniture = new ArrayList<Furniture>();
+	}
+	
+	public void run() {
+		while(true) {
+			repaint();
+			
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);;
+		
+		int width = getWidth();
+		int height = getHeight();
+		
+		double ratioX = (double)width/WIDTH;
+	    double ratioY = (double)height/HEIGHT;
+	    
+	    Graphics2D g2 = (Graphics2D)g;
+	    AffineTransform at = g2.getTransform();
+	    g2.scale(ratioX,ratioY);
+	    
+	    for(Furniture f : furniture) {
+	    	furniture.draw(g, this);
+	    }
+	    
+	    g2.setTransform(at);
 	}
 
 	@Override
