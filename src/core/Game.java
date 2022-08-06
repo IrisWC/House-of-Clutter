925478ed73d0d21c82b07305bcf6ed2cfd43f76a
Iris Chou <iris.w.chou@gmail.com>
2022-08-05 17:19:33 -0700

    Create Game.java

---
+++ src/core/Game.java
@@ -0,0 +1,45 @@
+package core;
+
+import java.awt.event.*;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements MouseListener {
+
+	public static final int WIDTH = 1600;
+	 public static final int HEIGHT = 900;
+	
+	public Game() {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
