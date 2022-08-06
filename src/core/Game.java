0699f1ec5bc95f05caf4f92ffbaff77f53501460
Monica Zhang <108095408+monica410@users.noreply.github.com>
2022-08-06 01:14:06 -0700

    f

--- src/core/Game.java
+++ src/core/Game.java
@@ -90,15 +90,15 @@ public class Game extends JPanel implements MouseListener {
 	private void addFurniture() {
 		//end 1300 pixels
 		//top line of furniture
-		furniture.add(new Furniture("Bedside.png", 5, 210, 125, 125));
-		furniture.add(new Furniture("Bed.png", 122, 216, 246, 246));
-		furniture.add(new Furniture("Dresser.png", 320, 115, 167, 257));
-		furniture.add(new Furniture("Dresser 3.png", 550, 110, 250, 310));
-		furniture.add(new Furniture("Block.png", 500, 200, 83, 92));
-		furniture.add(new Furniture("Bookshelf.png", 600, 500, 500, 600));
-		furniture.add(new Furniture("Pottery Shelf.png", 750, 450, 250, 450));
-		furniture.add(new Furniture("Cabinet.png", 800, 500, 320, 250));
-		furniture.add(new Furniture("Vanity.png", 1000, 500, 250, 350));
-		furniture.add(new Furniture("Table.png", 1200, 500, 380, 250));
+		furniture.add(new Furniture("Bedside.png", 3, 235, 95, 95));
+		furniture.add(new Furniture("Bed.png", 92, 216, 228, 228));
+		furniture.add(new Furniture("Dresser.png", 314, 160, 119, 180));
+		furniture.add(new Furniture("Dresser 3.png", 430, 120, 179, 221));
+		furniture.add(new Furniture("Block.png", 607, 260, 64, 70));
+		furniture.add(new Furniture("Bookshelf.png", 670, 75, 227, 272));
+		furniture.add(new Furniture("Pottery Shelf.png", 895, 162, 100, 180));
+		furniture.add(new Furniture("Cabinet.png", 994, 250, 114, 89));
+		furniture.add(new Furniture("Vanity.png", 1108, 190, 100, 140));
+		furniture.add(new Furniture("Table.png", 1208, 250, 89, 83));
 	}
 }
