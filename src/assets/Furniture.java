424e2fe8821dc669a6d6594e98d2521550465968
Iris Chou <iris.w.chou@gmail.com>
2022-08-05 17:31:33 -0700

    Update Furniture.java

--- src/assets/Furniture.java
+++ src/assets/Furniture.java
@@ -1,19 +1,23 @@
 package assets;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.ImageObserver;
 
 import javax.swing.ImageIcon;
 
 public class Furniture {
 	
-	private int x, y;
+	private int x, y, width, height;
 	private Image img;
 	private boolean removed;
 	
-	public Furniture(String image, int xcord, int ycord) {
+	public Furniture(String image, int xcord, int ycord, int w, int h) {
 		img = new ImageIcon(image).getImage();
 		x = xcord;
 		y = ycord;
+		width = w;
+		height = h;
 		removed = false;
 	}
 	
@@ -27,5 +31,10 @@ public class Furniture {
 	
 	public void remove() {
 		removed = true;
+	}
+	
+	public void draw(Graphics g, ImageObserver io) {
+		if (!removed)
+			g.drawImage(img, x, y, width, height, io);
 	}
 }
