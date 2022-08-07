d46a04270183f602af32a5327bf220a8840a35bc
Monica Zhang <108095408+monica410@users.noreply.github.com>
2022-08-06 17:57:56 -0700

    f

--- src/core/Game.java
+++ src/core/Game.java
@@ -204,6 +204,9 @@ public class Game extends JPanel implements MouseListener, MouseMotionListener {
 		furniture.add(new Furniture("Stool 3", 1132, 326, 57, 62));
 		//barrel
 		
+		//empty container
+		furniture.add(new Furniture("Bin", 723, 57, 51, 53));
+		furniture.add(new Furniture("Trough", 1036, 316, 95, 50));
 		//books
 		furniture.add(new Furniture("Books Stool", 861, 278, 50, 95));
 //		furniture.add(new Furniture("Books", 134, 41, 144, 139));
@@ -218,15 +221,23 @@ public class Game extends JPanel implements MouseListener, MouseMotionListener {
 		furniture.add(new Furniture("Picture 4", 424, 5, 52, 70));
 		furniture.add(new Furniture("Picture 5", 500, 55, 42, 56));
 		furniture.add(new Furniture("Picture", 529, 8, 63, 42));
-		furniture.add(new Furniture("Picture 2", 605, 49, 50, 69));
+		furniture.add(new Furniture("Picture 2", 605, 49, 60, 69));
 		//near bed stuff
 		furniture.add(new Furniture("Vase 3", 18, 212, 28, 44));
 		furniture.add(new Furniture("Lamp", 50, 195, 45, 63));
 		furniture.add(new Furniture("HorseR", 40, 400, 91, 79));
-		//dresser to pottery
+		//dresser to book
 		furniture.add(new Furniture("Chest", 393, 310, 81, 63));
+		furniture.add(new Furniture("Plates", 728, 42, 40, 40));
 		//near vanity stuff
 		furniture.add(new Furniture("Chest", 909, 142, 65, 50));
+		furniture.add(new Furniture("Blue Thing", 1151, 330, 34, 31));
+		furniture.add(new Furniture("Ink", 1263, 226, 23, 39));
+		furniture.add(new Furniture("Ink", 1272, 243, 23, 39));
+		furniture.add(new Furniture("Vase 2", 1231, 238, 42, 50));
+		furniture.add(new Furniture("Ink", 1265, 262, 23, 39));
+		furniture.add(new Furniture("Ink", 1214, 257, 23, 39));
+		furniture.add(new Furniture("Ink", 1227, 268, 23, 39));
 		//block cushions
 		//cushions
 		furniture.add(new Furniture("CushionOR", 150, 340, 47, 40));
